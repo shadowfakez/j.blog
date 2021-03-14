00e7db8d7fb1e682fe4774069fef1702ac88ec1f
shadowfakez <69105971+shadowfakez@users.noreply.github.com>
2021-03-14 21:27:44 +0200

    commit

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+
         return view('admin.index');
     }
 }
