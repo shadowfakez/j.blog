95daf4f37de602dd95279d47c8aacf678cad72c5
shadowfakez <69105971+shadowfakez@users.noreply.github.com>
2021-03-31 21:52:40 +0300

    added sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
@@ -26,5 +29,24 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+
+
+        view()->composer('layouts.sidebar', function($view) {
+
+            if (Cache::has('sidebar_categories')) {
+                $sidebar_categories = Cache::get('sidebar_categories');
+            } else {
+                $sidebar_categories = Category::withCount('posts')->orderBy
+                ('posts_count', 'desc')->get();
+                Cache::put('sidebar_categories', $sidebar_categories, 30);
+            }
+
+            $view->with('popular_posts', Post::orderBy('views', 'desc')->limit(6)->get());
+            $view->with('sidebar_categories', $sidebar_categories);
+        });
+
+
+
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,33 +8,25 @@
             </form>
         </div>
         <div class="gadget">
-            <h2 class="star"><span>Sidebar</span> Menu</h2>
+            <h2 class="star"><span>Популярные</span></h2>
             <div class="clr"></div>
-            <ul class="sb_menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">TemplateInfo</a></li>
-                <li><a href="#">Style Demo</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Archives</a></li>
-                <li><a href="#">Web Templates</a></li>
+            <ul class="ex_menu">
+                @foreach($popular_posts as $popular_post)
+                    <li><a href="{{ route('posts.single', ['slug' => $post->slug]) }}">{{ $popular_post->title }} | {{
+                $popular_post->views }}
+                    </a><br />
+                    {!! $popular_post->description !!} </li>
+                @endforeach
             </ul>
         </div>
         <div class="gadget">
-            <h2 class="star"><span>Sponsors</span></h2>
+            <h2 class="star"><span>Категории</span></h2>
             <div class="clr"></div>
-            <ul class="ex_menu">
-                <li><a href="#">Lorem ipsum dolor</a><br />
-                    Donec libero. Suspendisse bibendum</li>
-                <li><a href="#">Dui pede condimentum</a><br />
-                    Phasellus suscipit, leo a pharetra</li>
-                <li><a href="#">Condimentum lorem</a><br />
-                    Tellus eleifend magna eget</li>
-                <li><a href="#">Fringilla velit magna</a><br />
-                    Curabitur vel urna in tristique</li>
-                <li><a href="#">Suspendisse bibendum</a><br />
-                    Cras id urna orbi tincidunt orci ac</li>
-                <li><a href="#">Donec mattis</a><br />
-                    purus nec placerat bibendum</li>
+            <ul class="sb_menu">
+                @foreach($sidebar_categories as $sidebar_category)
+                    <li><a href="{{ route('categories.single', ['slug' => $sidebar_category->slug]) }}">{{
+                    $sidebar_category->title }} | ({{ $sidebar_category->posts_count }})</a></li>
+                @endforeach
             </ul>
         </div>
     </div>
